a85513b72c81d54a89fa958f70c246b4d3cef404
Atharva Thakur <atharvathakur369@gmail.com>
2024-06-27 04:21:41 +0530

    AUthentication Added

--- recipe/recipe-app-server/src/main/java/com/recipeApp/recipe/security/jwt/JwtUtils.java
+++ recipe/recipe-app-server/src/main/java/com/recipeApp/recipe/security/jwt/JwtUtils.java
@@ -23,13 +23,13 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${bezkoder.app.jwtSecret}")
+  @Value("${recipe.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${bezkoder.app.jwtExpirationMs}")
+  @Value("${recipe.app.jwtExpirationMs}")
   private int jwtExpirationMs;
 
-  @Value("${bezkoder.app.jwtCookieName}")
+  @Value("${recipe.app.jwtCookieName}")
   private String jwtCookie;
 
   public String getJwtFromCookies(HttpServletRequest request) {
