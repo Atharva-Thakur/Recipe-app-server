ec770083226b1ab4030f4ffb2adfe6ffc890f184
Atharva Thakur <atharvathakur369@gmail.com>
2024-07-03 22:39:12 +0530

    Minor fixes

--- recipe/recipe-app-server/src/main/java/com/recipeApp/recipe/controllers/RecipesController.java
+++ recipe/recipe-app-server/src/main/java/com/recipeApp/recipe/controllers/RecipesController.java
@@ -4,22 +4,25 @@ import com.recipeApp.recipe.models.Recipes;
 import com.recipeApp.recipe.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials = "true")
 @RequestMapping("/api/recipes")
 public class RecipesController {
 
     @Autowired
     private RecipeService recipeService;
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @GetMapping
     public List<Recipes> getAllRecipes() {
         return recipeService.getAllRecipes();
     }
+
 
     @GetMapping("/{id}")
     public Recipes getRecipeById(@PathVariable String id) {
